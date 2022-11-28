e6cab34a7a49a3f8ac430db526c820e7ee19cd87
David Murphy <David.Murphy3@aerlingus.com>
2022-11-28 22:57:03 +0000

    fix(commit-method): updated files for new feature

--- src/utils.ts
+++ src/utils.ts
@@ -81,7 +81,7 @@ function getClosedPRCommits(){
         core.info("Message is")
         core.info(JSON.stringify(context.payload.commits[0].message))
         for (let i in context.payload.commits){
-            core.info(context.payload.commits[i])
+            core.info(stringify(context.payload.commits[i]))
         }
         // commits = JSON.parse(JSON.stringify(context.payload.commits))
         if (pr_commit_count){
